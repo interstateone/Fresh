533b99f2e3efb3854a3a884e01a7882ea6296b11
Brandon Evans <brandon@brandonevans.ca>
2015-08-06 22:47:16 -0600

    Commit ./Pods with workaround for RAC and Mantle
    They don't seem to work well together because they both include
    libextobjc, so some includes in the RAC source got pointed at local
    paths

---
+++ Pods/Target Support Files/BSRefreshableScrollView/BSRefreshableScrollView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BSRefreshableScrollView : NSObject
+@end
+@implementation PodsDummy_BSRefreshableScrollView
+@end
---
+++ Pods/Target Support Files/RXCollections/RXCollections-umbrella.h
@@ -0,0 +1,7 @@
+#import <Cocoa/Cocoa.h>
+
+#import "RXCollection.h"
+
+FOUNDATION_EXPORT double RXCollectionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char RXCollectionsVersionString[];
+
