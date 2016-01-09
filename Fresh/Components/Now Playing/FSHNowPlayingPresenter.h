32ac30585925b1579af81cc61778bf34194cd948
Brandon Evans <brandon@brandonevans.ca>
2016-01-08 17:41:58 -0700

    Move now playing presenter properties into new view protocol

--- Fresh/Components/Now Playing/FSHNowPlayingPresenter.h
+++ Fresh/Components/Now Playing/FSHNowPlayingPresenter.h
@@ -12,31 +12,16 @@
 @class RACCommand;
 @class FSHAccount;
 @class FSHWaveform;
+@protocol NowPlayingView;
 
 @interface FSHNowPlayingPresenter : NSObject <SelectedSoundDelegate>
 
-@property (nonatomic, strong) NSString *title;
-@property (nonatomic, strong) NSString *author;
-@property (nonatomic, assign) BOOL playing;
-@property (nonatomic, assign) BOOL loading;
-@property (nonatomic, strong) NSNumber *duration;
-@property (nonatomic, strong) NSString *formattedDuration;
-@property (nonatomic, strong) NSNumber *progress;
-@property (nonatomic, strong) NSString *formattedProgress;
-@property (nonatomic, strong) NSURL *permalinkURL;
-@property (nonatomic, strong) FSHWaveform *waveform;
-@property (nonatomic, assign) BOOL favorite;
-@property (nonatomic, assign) BOOL hidden;
-
-@property (nonatomic, strong, readonly) RACCommand *toggleCurrentSound;
-@property (nonatomic, strong, readonly) RACCommand *toggleFavorite;
-
-@property (nonatomic, strong) NSViewController *view;
-
+@property (nonatomic, strong) id<NowPlayingView> view;
 @property (nonatomic, strong) FSHSound *sound;
 
+- (void)toggleCurrentSound;
+- (void)toggleFavorite;
 - (void)seekToProgress:(NSNumber *)progress;
-
 - (void)selectedSoundChanged:(FSHSound *)sound;
 
 @end
--- Fresh/Components/Now Playing/FSHNowPlayingViewController.h
+++ Fresh/Components/Now Playing/FSHNowPlayingViewController.h
@@ -7,9 +7,26 @@
 //
 
 @class FSHNowPlayingPresenter;
+@class FSHWaveform;
+@protocol NowPlayingView;
 
-@interface FSHNowPlayingViewController : NSTitlebarAccessoryViewController
+@interface FSHNowPlayingViewController : NSTitlebarAccessoryViewController <NowPlayingView>
 
 @property (nonatomic, strong) FSHNowPlayingPresenter *presenter;
+
+#pragma mark - NowPlayingView
+
+@property (nonatomic, copy) NSString *trackTitle;
+@property (nonatomic, copy) NSString *author;
+@property (nonatomic, assign) double progress;
+@property (nonatomic, assign) double duration;
+@property (nonatomic, copy) NSString *formattedProgress;
+@property (nonatomic, copy) NSString *formattedDuration;
+@property (nonatomic, assign) BOOL favorite;
+@property (nonatomic, assign) BOOL hidden;
+@property (nonatomic, strong) FSHWaveform *waveform;
+@property (nonatomic, assign) BOOL playing;
+@property (nonatomic, assign) BOOL loading;
+@property (nonatomic, strong) NSURL *permalinkURL;
 
 @end
